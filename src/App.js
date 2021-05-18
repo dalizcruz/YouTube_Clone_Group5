881be9df2344255d3c11ec5e6062d12a4b05f030
Daliz Cruz <dalizcruz@gmail.com>
2021-05-18 17:22:11 -0500

    navbar complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <Route exact path="/" component={Home} /> */}
+      {/* <Route path="/about" component={About} /> */}
+    </div>
+  );
+}
+
+export default App;
